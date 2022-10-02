4869b515551ee8df3e6ff2b5c35375a93f77449d
fragoba <franklin.gomez@o365.unab.edu.co>
2022-10-02 18:02:18 -0500

    Ejercicio03

--- main.js
+++ main.js
@@ -2,22 +2,30 @@ addEventListener("DOMContentLoaded", (e) => {
     let calcular = document.querySelector("#guia4");
     calcular.addEventListener("submit", (e) => {
         e.preventDefault();
-        let i = 1;
-        for(let j = 1; j < 21; j++){
-            if(j %2 != 0){
-                alert(i);
-                i += 4;
-            } 
-            if (j %2 == 0){
-                alert(i);
-                i -= 2;
-            } 
+        let cantEstudiantes = document.querySelector("#cantEstudiantes").value;
+        let notaFinal = 0, menor = 10, mayor = 0;
+        for(let i = 1; cantEstudiantes >= i; i++){
+            let calificacion = parseInt(prompt("Ingrese la calificación de la cafereria"));
+            notaFinal += calificacion;
+            if(calificacion > mayor){
+                mayor = calificacion;
+            } else if (calificacion < menor){
+                menor = calificacion;
+            } else {
+                menor = calificacion;
+            }
         }
+        let promedio = notaFinal / cantEstudiantes;
+        document.querySelector("#menor").innerHTML = `La calificación menor es: ${menor}`;
+        document.querySelector("#mayor").innerHTML = `La calificación mayor es: ${mayor}`; 
+        document.querySelector("#resultado").innerHTML = `La calificación promedio de la cafeteferia es: ${promedio}`
     })
 })
 
 function limpiar() {
     document.querySelector("#guia4").reset();
+    document.querySelector("#menor").innerHTML = "";
+    document.querySelector("#mayor").innerHTML = "";
     document.querySelector("#resultado").innerHTML = "";
 }
 
