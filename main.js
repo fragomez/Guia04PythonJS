a0e2dd83e1e1243b4eba47fe28d9964a98e50594
fragoba <franklin.gomez@o365.unab.edu.co>
2022-10-08 17:11:44 -0500

    Ejercicio08

--- main.js
+++ main.js
@@ -2,28 +2,29 @@ addEventListener("DOMContentLoaded", (e) => {
     let calcular = document.querySelector("#guia4");
     calcular.addEventListener("submit", (e) => {
         e.preventDefault();
-        let angulo = document.querySelector("#angulo").value;
-        let serie = document.querySelector("#serie").value;
-        let resultado = 0, numero = 3, factor = 1;
+        let cantNumeros = document.querySelector("#cantNumeros").value;
+        let numero = parseInt(prompt("Ingrese el número 1")).value;
+        let suma = numero, resta = numero, multiplicacion = numero, division = numero;
         for (let i = 1; i <= serie; i++) {
-            for (let j = 1; j <= numero; j++) {
-                factor *= j;
-            }
-            if (i % 2 == 0) {
-                resultado += (angulo * numero) / factor;
-                numero += 2;
-            } else {
-                resultado -= (angulo * numero) / factor;
-                numero += 2;
-            }
+            let numero2 = parseInt(prompt(`Ingrese el número ${i+1}`));
+            suma += numero2;
+            resta -= numero2;
+            multiplicacion *= numero2;
+            division /= numero2;
         }
-        document.querySelector("#resultado").innerHTML = `El seno del ángulo ${angulo} es: ${resultado}`;
+        document.querySelector("#suma").innerHTML = `El resultado de la suma es ${suma}`;
+        document.querySelector("#resta").innerHTML = `El resultado de la resta es ${suma}`;
+        document.querySelector("#multiplicacion").innerHTML = `El resultado de la multiplicación es ${suma}`;
+        document.querySelector("#division").innerHTML = `El resultado de la división es ${suma}`;
     })  
 })
 
 function limpiar() {
     document.querySelector("#guia4").reset();
-    document.querySelector("#resultado").innerHTML = "";
+    document.querySelector("#suma").innerHTML = "";
+    document.querySelector("#resta").innerHTML = "";
+    document.querySelector("#multiplicacion").innerHTML = "";
+    document.querySelector("#division").innerHTML = "";
 }
 
 addEventListener('reset', limpiar);
