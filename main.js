8132380cd15ec45c04385810fcb28ac496f1d03a
fragoba <franklin.gomez@o365.unab.edu.co>
2022-10-08 17:19:44 -0500

    Ejercicio09

--- main.js
+++ main.js
@@ -2,29 +2,25 @@ addEventListener("DOMContentLoaded", (e) => {
     let calcular = document.querySelector("#guia4");
     calcular.addEventListener("submit", (e) => {
         e.preventDefault();
-        let cantNumeros = document.querySelector("#cantNumeros").value;
-        let numero = parseInt(prompt("Ingrese el número 1")).value;
-        let suma = numero, resta = numero, multiplicacion = numero, division = numero;
-        for (let i = 1; i <= serie; i++) {
-            let numero2 = parseInt(prompt(`Ingrese el número ${i+1}`));
-            suma += numero2;
-            resta -= numero2;
-            multiplicacion *= numero2;
-            division /= numero2;
+        const iva = 0.16, descuento = 0.15, precio = 100000;
+        let total = 0;
+        let cantProductos = document.querySelector("#cantProductos").value;
+        for (let i = 1; i <= cantProductos; i++) {
+            total += precio;
         }
-        document.querySelector("#suma").innerHTML = `El resultado de la suma es ${suma}`;
-        document.querySelector("#resta").innerHTML = `El resultado de la resta es ${suma}`;
-        document.querySelector("#multiplicacion").innerHTML = `El resultado de la multiplicación es ${suma}`;
-        document.querySelector("#division").innerHTML = `El resultado de la división es ${suma}`;
+        let totalPagar = total(total * iva);
+        if (totalPagar > 500000){
+            let totalDescuento = totalPagar - (totalPagar * descuento);
+            document.querySelector("#resultado").innerHTML = `El total a pagar es: ${totalDescuento}`;
+        } else {
+            document.querySelector("#resultado").innerHTML = `El total a pagar es: ${totalPagar}`;
+        }
     })  
 })
 
 function limpiar() {
     document.querySelector("#guia4").reset();
-    document.querySelector("#suma").innerHTML = "";
-    document.querySelector("#resta").innerHTML = "";
-    document.querySelector("#multiplicacion").innerHTML = "";
-    document.querySelector("#division").innerHTML = "";
+    document.querySelector("#resultado").innerHTML = "";
 }
 
 addEventListener('reset', limpiar);
